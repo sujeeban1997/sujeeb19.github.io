c012cacda0ea189e415dd87a3610dcd5e6d0ea92
Sujeeban Gunathasan <53464105+sujeeban1997@users.noreply.github.com>
2024-02-22 01:01:33 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,42 @@
+// Navbar Add class function 
+$(window).scroll(function(){
+          if(this.scrollY > 20){
+              $('.navbar').addClass("sticky");
+          }else{
+              $('.navbar').removeClass("sticky");
+          }
+          if(this.scrollY > 500){
+              $('.scroll-up-btn').addClass("show");
+          }else {
+              $('.scroll-up-btn').removeClass("show");
+  
+          }
+      });
+
+//side up script
+$('.scroll-up-btn').click(function(){
+          $('html').animate({scrollTop: 0});
+      });
+  
+
+
+// Owl carousel for testimonial section 
+        $('.owl-carousel').owlCarousel({
+          loop:true,
+          margin:10,
+          autoplayTimeout: 5000,
+          smartSpeed:1000,
+          autoplay: false,
+          nav:false,
+          responsive:{
+              0:{
+                  items:1
+              },
+              600:{
+                  items:1
+              },
+              1000:{
+                  items:1
+              }
+          }
+      });
